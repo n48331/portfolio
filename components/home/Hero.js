1ee8c7cb792dcf7ae1c0642870252c9cb528a251
n48331 <unfolks@gmail.com>
2022-12-11 01:31:34 +0530

    error solution

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -11,44 +11,44 @@ export default function Hero() {
 			<div className='margin'>
 				<div className={styles['hero-content']}>
 					<div className={styles['hero-text-wrapper']}>
-						<h1>Hi, I'm Nabeel</h1>
+						<h1>Hi, I&apos;m Nabeel</h1>
 						<p>Full Stack Developer</p>
 						<div className={styles['cta-wrapper']}>
 							<a href='#' target='_blank'>
-								<button class='colorful-button'>
-									<div class='wrapper'>
+								<button className='colorful-button'>
+									<div className='wrapper'>
 										<span>Resume</span>
-										<div class='circle circle-12'></div>
-										<div class='circle circle-11'></div>
-										<div class='circle circle-10'></div>
-										<div class='circle circle-9'></div>
-										<div class='circle circle-8'></div>
-										<div class='circle circle-7'></div>
-										<div class='circle circle-6'></div>
-										<div class='circle circle-5'></div>
-										<div class='circle circle-4'></div>
-										<div class='circle circle-3'></div>
-										<div class='circle circle-2'></div>
-										<div class='circle circle-1'></div>
+										<div className='circle circle-12'></div>
+										<div className='circle circle-11'></div>
+										<div className='circle circle-10'></div>
+										<div className='circle circle-9'></div>
+										<div className='circle circle-8'></div>
+										<div className='circle circle-7'></div>
+										<div className='circle circle-6'></div>
+										<div className='circle circle-5'></div>
+										<div className='circle circle-4'></div>
+										<div className='circle circle-3'></div>
+										<div className='circle circle-2'></div>
+										<div className='circle circle-1'></div>
 									</div>
 								</button>
 							</a>
 							<Link href='/#projects' passHref>
-								<button class='colorful-button light'>
-									<div class='wrapper'>
+								<button className='colorful-button light'>
+									<div className='wrapper'>
 										<span>Projects</span>
-										<div class='circle circle-12'></div>
-										<div class='circle circle-11'></div>
-										<div class='circle circle-10'></div>
-										<div class='circle circle-9'></div>
-										<div class='circle circle-8'></div>
-										<div class='circle circle-7'></div>
-										<div class='circle circle-6'></div>
-										<div class='circle circle-5'></div>
-										<div class='circle circle-4'></div>
-										<div class='circle circle-3'></div>
-										<div class='circle circle-2'></div>
-										<div class='circle circle-1'></div>
+										<div className='circle circle-12'></div>
+										<div className='circle circle-11'></div>
+										<div className='circle circle-10'></div>
+										<div className='circle circle-9'></div>
+										<div className='circle circle-8'></div>
+										<div className='circle circle-7'></div>
+										<div className='circle circle-6'></div>
+										<div className='circle circle-5'></div>
+										<div className='circle circle-4'></div>
+										<div className='circle circle-3'></div>
+										<div className='circle circle-2'></div>
+										<div className='circle circle-1'></div>
 									</div>
 								</button>
 							</Link>
--- components/home/Skills.js
+++ components/home/Skills.js
@@ -55,24 +55,24 @@ export default function Skills() {
 					</div>
 				</div>
 				<Link href='/#projects' passHref>
-					<button class='colorful-button light'>
-						<div class='wrapper'>
+					<button className='colorful-button light'>
+						<div className='wrapper'>
 							<div className={styles['button_text']}>
 								Projects
 								<AiOutlineArrowRight />
 							</div>
-							<div class='circle circle-12'></div>
-							<div class='circle circle-11'></div>
-							<div class='circle circle-10'></div>
-							<div class='circle circle-9'></div>
-							<div class='circle circle-8'></div>
-							<div class='circle circle-7'></div>
-							<div class='circle circle-6'></div>
-							<div class='circle circle-5'></div>
-							<div class='circle circle-4'></div>
-							<div class='circle circle-3'></div>
-							<div class='circle circle-2'></div>
-							<div class='circle circle-1'></div>
+							<div className='circle circle-12'></div>
+							<div className='circle circle-11'></div>
+							<div className='circle circle-10'></div>
+							<div className='circle circle-9'></div>
+							<div className='circle circle-8'></div>
+							<div className='circle circle-7'></div>
+							<div className='circle circle-6'></div>
+							<div className='circle circle-5'></div>
+							<div className='circle circle-4'></div>
+							<div className='circle circle-3'></div>
+							<div className='circle circle-2'></div>
+							<div className='circle circle-1'></div>
 						</div>
 					</button>
 				</Link>
