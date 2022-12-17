2b7f35062e025727ccd2ec545be13e740316404e
n48331 <unfolks@gmail.com>
2022-12-17 19:52:36 +0530

    all

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -14,51 +14,20 @@ export default function Hero() {
 						<h1>Hi, I&apos;m Nabeel</h1>
 						<p>Full Stack Developer</p>
 						<div className={styles['cta-wrapper']}>
-							<a href='#' target='_blank'>
-								<button className='colorful-button'>
-									<div className='wrapper'>
-										<span>Resume</span>
-										<div className='circle circle-12'></div>
-										<div className='circle circle-11'></div>
-										<div className='circle circle-10'></div>
-										<div className='circle circle-9'></div>
-										<div className='circle circle-8'></div>
-										<div className='circle circle-7'></div>
-										<div className='circle circle-6'></div>
-										<div className='circle circle-5'></div>
-										<div className='circle circle-4'></div>
-										<div className='circle circle-3'></div>
-										<div className='circle circle-2'></div>
-										<div className='circle circle-1'></div>
-									</div>
-								</button>
-							</a>
 							<Link href='/#projects' passHref>
-								<button className='colorful-button light'>
-									<div className='wrapper'>
-										<span>Projects</span>
-										<div className='circle circle-12'></div>
-										<div className='circle circle-11'></div>
-										<div className='circle circle-10'></div>
-										<div className='circle circle-9'></div>
-										<div className='circle circle-8'></div>
-										<div className='circle circle-7'></div>
-										<div className='circle circle-6'></div>
-										<div className='circle circle-5'></div>
-										<div className='circle circle-4'></div>
-										<div className='circle circle-3'></div>
-										<div className='circle circle-2'></div>
-										<div className='circle circle-1'></div>
-									</div>
-								</button>
+								<button className='button resume'>Resume</button>
+							</Link>
+
+							<Link href='/#projects' passHref>
+								<button className='button'>Projects</button>
 							</Link>
 						</div>
 					</div>
-					{/* <Lottie
+					<Lottie
 						animationData={groovyWalkAnimation}
 						loop={true}
 						className={styles['hero-image']}
-					/> */}
+					/>
 					{/* <img src='/images/hero.svg' className={styles['hero-image']} /> */}
 				</div>
 			</div>
--- components/home/Skills.js
+++ components/home/Skills.js
@@ -55,24 +55,12 @@ export default function Skills() {
 					</div>
 				</div>
 				<Link href='/#projects' passHref>
-					<button className='colorful-button light'>
+					<button className='button'>
 						<div className='wrapper'>
 							<div className={styles['button_text']}>
 								Projects
 								<AiOutlineArrowRight />
 							</div>
-							<div className='circle circle-12'></div>
-							<div className='circle circle-11'></div>
-							<div className='circle circle-10'></div>
-							<div className='circle circle-9'></div>
-							<div className='circle circle-8'></div>
-							<div className='circle circle-7'></div>
-							<div className='circle circle-6'></div>
-							<div className='circle circle-5'></div>
-							<div className='circle circle-4'></div>
-							<div className='circle circle-3'></div>
-							<div className='circle circle-2'></div>
-							<div className='circle circle-1'></div>
 						</div>
 					</button>
 				</Link>
